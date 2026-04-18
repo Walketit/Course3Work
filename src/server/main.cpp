191485ae24a38b660bdbe7e44faea2cd0dea4b96
Walketit <walkerdmit@yandex.ru>
2026-04-18 17:58:34 +0700

    feat: integrate database initialization

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,6 +1,20 @@
 #include "common/Logger.h"
+#include "server/DatabaseManager.h"
 
 int main() {
     Logger::getInstance().log("Сервер запускается...");
+
+    // Инициализация базы данных
+    if (!DatabaseManager::getInstance().init("messenger.db")) {
+        Logger::getInstance().log("Критическая ошибка: невозможно инициализировать БД. Остановка сервера.", LogLevel::ERROR);
+        return 1;
+    }
+
+    // Имитация работы сервера
+    Logger::getInstance().log("Ожидание подключений (пока не реализовано)...", LogLevel::DEBUG);
+
+    // Перед выходом корректно закрываем БД
+    DatabaseManager::getInstance().close();
+    
     return 0;
 }
